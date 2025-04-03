b77b9f26c3f9c12e21bd6cb3ac8b1aa6947bb496
ezequieltss <163845128+ezequieltss@users.noreply.github.com>
2025-04-03 11:46:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function calcular() {
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
 
-    let operacao = document.getElementById("operacao").onvolumechange;
+    let operacao = document.getElementById("operacao").value;
     let resultado;
 
     if (isNaN(num1) || isNaN(num2)) {
